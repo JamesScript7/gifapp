1670835d1d8ef2e16248fe353e27100b38f1846b
James Kim <james.kim7@yahoo.com>
2017-03-04 16:38:42 -0500

    got stream to work

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log("hello world!");
+var constraints = {
+	audio: false,
+	video: true
+}
+
+function success(strm) {
+	var video = document.querySelector('video');
+	video.src = window.URL.createObjectURL(strm);
+}
+
+function error(err) {
+	console.log("Error: ", err);
+}
+
+
+navigator.getUserMedia(constraints, success, error);
