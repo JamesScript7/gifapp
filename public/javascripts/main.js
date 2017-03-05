49c9a21452f3c28fdfaa69f273b0ec0a2d0c884c
James Kim <james.kim7@yahoo.com>
2017-03-05 17:20:34 -0500

    sunday push

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,73 +6,95 @@ navigator.mediaDevices.getUserMedia({
 	video: true
 }).then(function success(stream) {
 	var video = document.querySelector('video');
-
 	video.src = window.URL.createObjectURL(stream) || stream;
-
 }).catch(function error(err) {
 	console.error("Error:", err);
 });
-
 
 // GIF settings
 
 (function(window, document) {
 	var
-		gifHeight = document.querySelector('.video-stream').clientHeight,
-		gifWidth = document.querySelector('.video-stream').clientWidth,
+		// gifWidth = document.querySelector('.video-stream').clientWidth,
 		text = document.getElementById('text'),
 		getSelectedOptions = function() {
 			return {
-				'gifWidth': gifWidth,
-			  'gifHeight': gifHeight,
-			  'numFrames': '12',
+				'gifWidth': '300',
+			  'gifHeight': '225',
+			  'numFrames': '16',
 			  'text': text.value,
 			  'fontSize': '24px',
 			  'fontFamily': 'Calibri',
 			  'fontColor': '#FFFFFF'
 			}
 		},
+	  passedOptions,
+	  updateCodeBlock = function(obj) {
+	  	obj = obj || {};
+	  	var
+	  		targetElem = obj.targetElem,
+	  		selectedOptions = getSelectedOptions(),
+	  		options = (function() {
+	  			var obj = {};
+	  			_.each(selectedOptions, function(val, key) {
+	  				if(val) {
+	  					obj[key] = val;
+	  				}
+	  			});
+	  			return obj;
+	  		}())
+	  },
 	// Swapping the video stream with the recorded GIF
 		createGif = document.getElementById('create-gif-btn'),
 	  gifDisplay = document.querySelector('.gif-display'),
 	  videoStream = document.querySelector('.video-stream'),
-	  passedOptions;
+	  progressBar = document.querySelector('progress'),
+	  bindEvents = function() {
+			createGif.addEventListener('click', function(e) {
+				passedOptions = _.merge(_.clone(getSelectedOptions()), {
+					'progressCallBack': function(captureProgress) {
+						progressBar.value = captureProgress;
 
-	console.log("Height:", gifHeight);
-	console.log("Width:", gifWidth);
+					}
+				});
+				gifshot.createGIF(passedOptions, function(obj) {
+					if (!obj.error) {
+						var
+							image = obj.image,
+							animatedImage = document.createElement('img');
+							animatedImage.src = image;
 
-	createGif.addEventListener('click', function(e) {
-		var gifHeight = document.querySelector('.video-stream').clientHeight;
+							videoStream.classList.add('hidden');
 
-		passedOptions = _.merge(_.clone(getSelectedOptions()), {
-			'progressCallBack': function(captureProgress) {
-				console.log(captureProgress);
-			}
-		});
+							gifDisplay.appendChild(animatedImage);
+							gifDisplay.classList.remove('hidden');
+					} else {
+						console.log('obj.error', obj.error);
+						console.log('obj.errorCode', obj.errorCode);
+						console.log('obj.errorMsg', obj.errorMsg);
+					}
+				});
+			}, false);
 
-		gifshot.createGIF(passedOptions, function(obj) {
-			if (!obj.error) {
-				var
-					image = obj.image,
-					animatedImage = document.createElement('img');
-					animatedImage.src = image;
+			document.addEventListener('change', function(e) {
+        updateCodeBlock({
+          targetElem: e.target
+        });
+        // console.log(getSelectedOptions({})["progressCallBack"]);
+        console.log(updateCodeBlock);
+      });
 
-					videoStream.classList.add('hidden');
+      document.addEventListener('keyup', function(e) {
+        updateCodeBlock({
+          targetElem: e.target
+        });
+      });
 
-					gifDisplay.appendChild(animatedImage);
-					gifDisplay.classList.remove('hidden');
-			} else {
-				console.log('obj.error', obj.error);
-				console.log('obj.errorCode', obj.errorCode);
-				console.log('obj.errorMsg', obj.errorMsg);
-			}
-		});
-	}, false);
+	};
 
+	bindEvents();
+	updateCodeBlock();
 }(window, document));
-
-
-
 
 
 
